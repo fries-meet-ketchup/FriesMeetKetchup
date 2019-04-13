38f406249343ff5fedca3bec79e8073fea88ecb0
HibaAl <49596965+HibaAl@users.noreply.github.com>
2019-04-13 15:48:57 -0600

    Update project.cpp

--- project.cpp
+++ project.cpp
@@ -3,3 +3,93 @@
 
 using namespace std;
 
+
+  
+Heap::Heap(int queueSize){
+  maxQueueSize = queueSize;
+  currentQueueSize = 0;
+  priorityQueue = new Node[queueSize];
+}
+
+Heap::~Heap(){
+  delete [] priorityQueue;
+}
+
+void swap(Node *x, Node *y)
+{
+    Node temp = *x;
+    *x = *y;
+    *y = temp;
+}
+
+int parent(int index)
+{
+ return (index-1)/2;
+}
+
+int leftChild(int index)
+{
+ return ((2*index) + 1);
+}
+
+int rightChild(int index)
+{
+ return ((2*index) + 2);
+}
+
+void Heap::enqueue(string, string, string, string, string, string, double){
+  
+}
+
+void Heap::dequeue(){
+  
+}
+
+Node Heap::peek(){
+  
+}
+
+bool Heap::isFull(){
+  if(currentQueueSize != maxQueueSize){
+    return false;
+  }
+  else{
+    return true;
+  }
+}
+  
+bool Heap::isEmpty(){
+  if(currentQueueSize == 0){
+    return true;
+  }
+  else{
+    return false;
+  }
+}
+
+void Heap::repairUpward(int nodeIndex)
+{
+  while (nodeIndex != 0 && (priorityQueue[parent(nodeIndex)] > priorityQueue[nodeIndex])){
+    swap(&priorityQueue[nodeIndex], &priorityQueue[parent(nodeIndex)]);
+    nodeIndex = parent(nodeIndex);
+  }
+}
+
+void Heap::repairDownward(int nodeIndex)
+{
+  int right = rightChild(nodeIndex);
+  int left = leftChild(nodeIndex);
+  int smallest = nodeIndex;
+  if(left< currentSize && ((priorityQueue[left] < priorityQueue[nodeIndex]) || ( priorityQueue[left] == priorityQueue[nodeIndex])))
+  {
+    smallest = left;
+  }
+  if(right < currentSize &&((priorityQueue[right] < priorityQueue[smallest]) || ( priorityQueue[right] == priorityQueue[smallest])))
+  {
+    smallest = right;
+  }
+  if(smallest != nodeIndex){
+    swap(&priorityQueue[nodeIndex], &priorityQueue[smallest]);
+    nodeIndex = smallest;
+  }
+}
