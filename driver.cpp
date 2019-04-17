d44140ee75db6979dc08ab5735375d90e853cfb6
go6342410 <49596935+go6342410@users.noreply.github.com>
2019-04-17 12:53:08 -0600

    Update driver.cpp
    hjk

--- driver.cpp
+++ driver.cpp
@@ -6,11 +6,11 @@ using namespace std;
 void menu()
 {
   cout << "============Main Menu============" << endl;
-  cout << "1. Get group information from file" << endl;
-  cout << "2. Add a group to Priority Queue" << endl;
-  cout << "3. Show next group in the queue" << endl;
-  cout << "4. Serve Next group" << endl;
-  cout << "5. Serve Entire Queue" << endl;
+  cout << "1. Choose the country of food: American, Japanese, Chinese, Korean, Mediterranean, Italian, Indian, Mexican." << endl;
+  cout << "2. Choose the city: Boulder, Westminster, Denver." << endl;
+  cout << "3. Choose the type of food: Brunch, Coffee, Restaurant." << endl;
+  cout << "4. Are you vegeterian or not: Y or N." << endl;
+  cout << "5. Choose the price: $ or $$, or $$$" << endl;
   cout << "6. Quit" << endl;
 }
 
