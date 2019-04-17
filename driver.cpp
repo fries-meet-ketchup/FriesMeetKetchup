6c62a73ceb6c293ec8fecd45c37c258c69f48ddf
HibaAl <49596965+HibaAl@users.noreply.github.com>
2019-04-17 12:44:58 -0600

    Update driver.cpp

--- driver.cpp
+++ driver.cpp
@@ -3,3 +3,20 @@
 
 using namespace std;
 
+void menu()
+{
+  cout << "============Main Menu============" << endl;
+  cout << "1. Get group information from file" << endl;
+  cout << "2. Add a group to Priority Queue" << endl;
+  cout << "3. Show next group in the queue" << endl;
+  cout << "4. Serve Next group" << endl;
+  cout << "5. Serve Entire Queue" << endl;
+  cout << "6. Quit" << endl;
+}
+
+int main(int argc, char* argv[])
+{
+  
+  
+  
+}
