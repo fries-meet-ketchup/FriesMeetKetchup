fad5a86aeaa4569c195e95ce669157aece61dc7f
HibaAl <49596965+HibaAl@users.noreply.github.com>
2019-04-18 12:40:57 -0600

    Update driver.cpp

--- driver.cpp
+++ driver.cpp
@@ -3,16 +3,16 @@
 
 using namespace std;
 
-void menu()
-{
-  cout << "============Main Menu============" << endl;
-  cout << "1. Choose the country of food: American, Japanese, Chinese, Korean, Mediterranean, Italian, Indian, Mexican." << endl;
-  cout << "2. Choose the city: Boulder, Westminster, Denver." << endl;
-  cout << "3. Choose the type of food: Brunch, Coffee, Restaurant." << endl;
-  cout << "4. Are you vegeterian or not: Y or N." << endl;
-  cout << "5. Choose the price: $ or $$, or $$$" << endl;
-  cout << "6. Quit" << endl;
-}
+// void menu()
+// {
+//   cout << "============Main Menu============" << endl;
+//   cout << "1. Choose the country of food: American, Japanese, Chinese, Korean, Mediterranean, Italian, Indian, Mexican." << endl;
+//   cout << "2. Choose the city: Boulder, Westminster, Denver." << endl;
+//   cout << "3. Choose the type of food: Brunch, Coffee, Restaurant." << endl;
+//   cout << "4. Are you vegeterian or not: Y or N." << endl;
+//   cout << "5. Choose the price: $ or $$, or $$$" << endl;
+//   cout << "6. Quit" << endl;
+// }
 
 int main(int argc, char* argv[])
 {
@@ -73,33 +73,11 @@ int main(int argc, char* argv[])
         getline(str, ci, ',');
         city = ci;
         if(country == answerCountry && type == answerType && city == answerCity && Vegeterian == answerVeg && price == answerPrice){
-          pq.enqueue(country, , );
+          pq.enqueue(restaurant, rating);
         }
-        else{
-          count++;
-          break;
-        }
-      }
-    }
-  }
-  while (choice != "7")
-  {
-    menu();
-    getline(cin, choice);
-    
-      case 6:
-      {
-        cout << "Goodbye!" << endl;
-        return 0;
-      }
-      default:
-      {
-        cout << "invalid input" << endl<<endl;
-        break;
       }
     }
   }
   inStream.close();
-  menu();
   return 0;
 }
