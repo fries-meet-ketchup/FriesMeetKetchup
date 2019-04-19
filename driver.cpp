90290dca152efcaa05192817021d046a367f0093
HibaAl <49596965+HibaAl@users.noreply.github.com>
2019-04-19 16:23:44 -0600

    Update driver.cpp

--- driver.cpp
+++ driver.cpp
@@ -1,22 +1,17 @@
+#include <vector>
+#include <queue>
+#include <string>
+#include <fstream>
 #include <iostream>
+#include <sstream>
 #include "project.hpp"
 
 using namespace std;
 
-// void menu()
-// {
-//   cout << "============Main Menu============" << endl;
-//   cout << "1. Choose the country of food: American, Japanese, Chinese, Korean, Mediterranean, Italian, Indian, Mexican." << endl;
-//   cout << "2. Choose the city: Boulder, Westminster, Denver." << endl;
-//   cout << "3. Choose the type of food: Brunch, Coffee, Restaurant." << endl;
-//   cout << "4. Are you vegeterian or not: Y or N." << endl;
-//   cout << "5. Choose the price: $ or $$, or $$$" << endl;
-//   cout << "6. Quit" << endl;
-// }
-
 int main(int argc, char* argv[])
 {
-  Heap pq(stoi(argv[1]));
+  int n = 6;
+  Heap pq(n);
   string country;
   string city;
   string type;
@@ -39,7 +34,7 @@ int main(int argc, char* argv[])
   getline(cin, answerCity);
   cout << "3. Choose the type of food: Brunch, Coffee, Restaurant." << endl;
   getline(cin, answerType);
-  cout << "4. Are you vegeterian or not: Yes or No." << endl;
+  cout << "4. Are you vegeterian or not: Y or N." << endl;
   getline(cin, answerVeg);
   cout << "5. Choose the price: $ or $$, or $$$" << endl;
   getline(cin, answerPrice);
@@ -50,34 +45,52 @@ int main(int argc, char* argv[])
     while (getline(inStream,line)){
       if(line != "")
       {
-        stringstream str;
+        // cout << line << endl;
+        stringstream str(line);
         string t;
         getline(str, t, ',');
         type = t;
+        cout<<"Testing: "<<type<<" rest. "<<endl;
         string r;
         getline(str, r, ',');
         restaurant = r;
+        cout<<"Testing: "<<restaurant<<" type. "<<endl;
+
         string v;
         getline(str, v, ',');
         vegeterian = v;
+        cout<<"Testing: "<<vegeterian<<" veg. "<<endl;
+
         string c;
         getline(str, c, ',');
         country = c;
-        string rt;
-        getline(str, rt, ',');
-        rating = rt;
+        cout<<"Testing: "<<country<<" country "<<endl;
+        getline(str, rating, ',');
+        cout<<"Testing: "<<rating<<" rating "<<endl;
+
         string p;
         getline(str, p, ',');
         price = p;
-        string ci;
-        getline(str, ci, ',');
-        city = ci;
-        if(country == answerCountry && type == answerType && city == answerCity && Vegeterian == answerVeg && price == answerPrice){
-          pq.enqueue(restaurant, rating);
+        cout<<"Testing: "<<price<<" price "<<endl;
+
+        getline(str, city,',');
+
+        cout<<"Testing: "<<city<<" city "<<endl;
+
+        // cout<<"Testing: "<<type;
+        // cout << country << "&" <<  answerCountry << endl;
+        // cout<<  type << "&" << answerType << endl;
+        // cout << city <<"&" << answerCity <<endl;
+        // cout << vegeterian << "&" << answerVeg << endl;
+        // cout <<price << "&" <<answerPrice << endl;
+        if(country == answerCountry && type == answerType && city == answerCity && vegeterian == answerVeg && price == answerPrice){
+          pq.enqueue(restaurant, stod(rating));
         }
       }
     }
+    pq.printArray(n);
   }
   inStream.close();
   return 0;
 }
+
